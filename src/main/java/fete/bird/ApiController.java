3c7d4c065870f4a97cda52532cc1cf902a672be7
anandjaisy <anandjaisy@gmail.com>
2023-05-21 14:31:48 +1000

    Intial setup

---
+++ src/main/java/fete/bird/ApiController.java
@@ -0,0 +1,12 @@
+package fete.bird;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/api")
+public class ApiController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Example Response";
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module api.main {
+    requires io.micronaut.context;
+    requires io.swagger.v3.oas.annotations;
+    requires io.micronaut.http;
+}
